fc2bb7da81b2c6eeaf2b2c657b2808e1af53ac83
Callum Rogers <callumr@palantir.com>
2016-02-01 18:52:11 +0000

    Code for part1

--- src/snake.ts
+++ src/snake.ts
@@ -5,15 +5,35 @@ import * as Bacon from 'baconjs'
 
 import { Stream, Property } from './bacon-extras.ts'
 import { Position } from './position.ts'
+import { Direction } from './direction.ts'
 import { Keys } from './inputs.ts'
 import { Snake } from './types.ts'
 
+function equalTo<T>(expected: T) {
+    return (actual: T) => actual === expected
+}
 
+function snakeHeadPosition(
+    initialHeadPosition: Position,
+    keyPresses: Stream<Keys>): Property<Position> {
+
+    const ups: Stream<Keys> = keyPresses.filter(equalTo(Keys.UP));
+
+    const headPostion: Property<Position> = ups.scan(initialHeadPosition, (lastHeadPosition, upKeyPress) => {
+        return lastHeadPosition.advance(Direction.up())
+    });
+
+    return headPostion;
+}
 
 export function snake(width: number, height: number, keyPresses: Stream<Keys>): Property<Snake> {
-    return Bacon.constant({
-        head: Position.at(3, 5),
-        tail: [],
+    const initialPosition = Position.at(3, 5);
+    const headPosition = snakeHeadPosition(initialPosition, keyPresses);
+    const snakeRenderData = Bacon.combineTemplate({
+        head: headPosition,
+        tail: Bacon.constant([]),
         food: null
     });
+
+    return snakeRenderData;
 }
